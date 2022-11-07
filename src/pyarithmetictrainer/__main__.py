10f80e0ba9debb86f22216cfc1e61e70ede232d8
詹詹 <50549207+liushuchen2025@users.noreply.github.com>
2022-11-06 21:02:36 -0500

    Implemented a basic main function

--- src/pyarithmetictrainer/__main__.py
+++ src/pyarithmetictrainer/__main__.py
@@ -2,4 +2,14 @@ import wisdom as wisdom
 
 
 def main():
-    return 0
+    while True:
+        question = wisdom.generate_question()
+        wisdom.print_prompt(question[0], question[1], question[2])
+        ans = int(input("Enter Your Answer: "))
+        if (ans == question[3]):
+            print("Correct")
+        else:
+            print("Incorrect")
+
+if __name__ == "__main__":
+    main()
--- src/pyarithmetictrainer/wisdom.py
+++ src/pyarithmetictrainer/wisdom.py
@@ -27,3 +27,56 @@ def cube_root(a):
 
 def modulus(a, b):
     return a % b
+
+
+def generate_question():
+    """Generate a random question."""
+    question = random.choice([add, subtract, multiply, divide, sqr_root, modulus])
+    if question == add:
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        ans = add(a, b)
+        return question, a, b, ans
+    elif question == subtract:
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        ans = subtract(a, b)
+        return question, a, b, ans
+    elif question == multiply:
+        a = random.randint(1, 10)
+        b = random.randint(1, 10)
+        ans = multiply(a, b)
+        return question, a, b, ans
+    elif question == divide:
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        ans = divide(a, b)
+        return question, a, b, ans
+    elif question == sqr_root:
+        a = random.randint(1, 100)
+        ans = sqr_root(a)
+        return question, a, ans
+    elif question == modulus:
+        a = random.randint(1, 100)
+        b = random.randint(1, 100)
+        ans = modulus(a, b)
+        return question, a, b, ans
+
+
+def print_prompt(question, a, b):
+    print("What is the answer to the following question?")
+    if question == add:
+        print(f"{a} + {b} = ")
+    elif question == subtract:
+        print(f"{a} - {b} = ")
+    elif question == multiply:
+        print(f"{a} * {b} = ")
+    elif question == divide:
+        print(f"{a} / {b} = ")
+    elif question == sqr_root:
+        print(f"√{a} = ")
+    elif question == modulus:
+        print(f"{a} % {b} = ")
+
+
+
