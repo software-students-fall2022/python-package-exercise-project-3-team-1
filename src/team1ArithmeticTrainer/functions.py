b51929093660c5b85c16e29e669d04af99fa7f95
Kevin <kevinxgong@gmail.com>
2022-11-07 13:57:59 -0500

    Added addition function

--- src/team1ArithmeticTrainer/functions.py
+++ src/team1ArithmeticTrainer/functions.py
@@ -1,5 +1,14 @@
-def addition(self, minLim, maxLim):
+import random
 
+def addition(self, minLim, maxLim):
+    numOne = random.randInt(minLim, maxLim)
+    numTwo = random.randInt(minLim, maxLim)
+    solution = numOne + numTwo
+    answer = input("What is" + str(numOne) + " + " + str(numTwo) + ":")
+    if(int(answer) == (numOne + numTwo)):
+        print("Correct!")
+    else:
+        print("Incorrect, the answer is: " + str(solution))
 def subtraction(self, minLim, maxLim):
 
 def multiplication(self, minLim, maxLim):
