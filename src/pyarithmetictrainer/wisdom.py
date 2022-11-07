041e0c51019972728a9294049dfab06bb7b37b64
詹詹 <50549207+liushuchen2025@users.noreply.github.com>
2022-11-06 20:04:13 -0500

    Completed add, sub, div, multiplication, square root, divide, and mod

---
+++ src/pyarithmetictrainer/wisdom.py
@@ -0,0 +1,29 @@
+import random
+
+
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+def multiply(a, b):
+    return a * b
+
+
+def divide(a, b):
+    return a / b
+
+
+def sqr_root(a):
+    return a ** 0.5
+
+
+def cube_root(a):
+    return a ** (1/3)
+
+
+def modulus(a, b):
+    return a % b
