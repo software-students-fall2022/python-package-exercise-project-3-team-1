49a69a54c21c556f023ecb124c08cf3d8e5a87cb
Dixit Timilsina <dt1930@nyu.edu>
2022-11-07 14:44:45 -0500

    "Updated multiplication"

--- src/team1ArithmeticTrainer/__main__.py
+++ src/team1ArithmeticTrainer/__main__.py
@@ -2,6 +2,6 @@ import functions
 
 def main():
     #while loop for arithmetic trainer
-
+    return 0
 if __name__ == '__main__':
     main()
--- src/team1ArithmeticTrainer/functions.py
+++ src/team1ArithmeticTrainer/functions.py
@@ -1,11 +1,32 @@
+import random
+
 def addition(self, minLim, maxLim):
-
+    numOne = random.randInt(minLim, maxLim)
+    numTwo = random.randInt(minLim, maxLim)
+    solution = numOne + numTwo
+    answer = input("What is" + str(numOne) + " + " + str(numTwo) + ":")
+    if(int(answer) == (numOne + numTwo)):
+        print("Correct!")
+    else:
+        print("Incorrect, the answer is: " + str(solution))
 def subtraction(self, minLim, maxLim):
-
+    return 0
 def multiplication(self, minLim, maxLim):
-
+    num1=random.randint(minLim,maxLim)
+    num2=random.randint(minLim,maxLim)
+    answer=num1*num2
+    answer = input("What is" + str(num1) + " * " + str(num2) + ":")
+    userInput=input()
+    try:
+        if int(userInput)==answer:
+            return True
+        else:
+            return False
+    except:
+        return "Invalid"
 def squareRoot(self, minLim, maxLim):
-
+    return 0
 def divion(self, minLim, maxLim):
-
-def modulus(self, minLim, maxLim):
+    return 0
+def modulus(self, minLim, maxLim): 
+    return 0
