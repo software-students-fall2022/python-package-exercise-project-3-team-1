465cf9a23daaad3d1a82b172d77105aa07dbf609
詹詹 <50549207+liushuchen2025@users.noreply.github.com>
2022-11-12 17:28:43 -0500

    fixed typo

--- src/team1ArithmeticTrainer/functions.py
+++ src/team1ArithmeticTrainer/functions.py
@@ -3,7 +3,7 @@ import random
 def checkPerfectSquare(num):
     if num == 0:
         return 0
-    if num0:
+    if num>0:
         for i in range(1, num+1):
             if i*i==num:
                 return i
